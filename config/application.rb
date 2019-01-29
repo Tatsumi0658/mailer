0c76ee01b9bf2e8dc0e4dcde5eca1522681dc607
koda <hogehogehoge@gmail.com>
2019-01-30 00:58:05 +0900

    デバッグツールを導入する

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Mailer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    cpnfig.i18n.default_locale = :ja
+
     config.generators do |g|
       g.assets false
       g.helper false
