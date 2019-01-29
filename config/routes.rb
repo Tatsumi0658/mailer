059a5c1bb65a66c4651400e901427e35eaa54b2e
koda <hogehogehoge@gmail.com>
2019-01-30 00:43:59 +0900

    不要なassets,helperが作成されないように設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :contacts
+  # resources :contacts
   resources :contents
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
